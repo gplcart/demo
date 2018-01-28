00dbf2d97200c4da7fa5dc7803dc973c9f001074
GplCart <gplcart.software@gmail.com>
2018-01-28 17:47:35 +0200

    Update config

--- config/default/collection_item.php
+++ config/default/collection_item.php
@@ -10,37 +10,37 @@ return array(
     array(
         'weight' => 0,
         'status' => 1,
-        'value' => $this->created['file']['Tissot watches'],
+        'entity_id' => $this->created['file']['Tissot watches'],
         'collection_id' => $this->created['collection']['Banners']
     ),
     array(
         'weight' => 1,
         'status' => 1,
-        'value' => $this->created['file']['Casio watches'],
+        'entity_id' => $this->created['file']['Casio watches'],
         'collection_id' => $this->created['collection']['Banners']
     ),
     array(
         'weight' => 0,
         'status' => 1,
-        'value' => $this->created['product']['EF527D-1AV'],
+        'entity_id' => $this->created['product']['EF527D-1AV'],
         'collection_id' => $this->created['collection']['Featured products']
     ),
     array(
         'weight' => 1,
         'status' => 1,
-        'value' => $this->created['product']['EQS500DB-1A1'],
+        'entity_id' => $this->created['product']['EQS500DB-1A1'],
         'collection_id' => $this->created['collection']['Featured products']
     ),
     array(
         'weight' => 2,
         'status' => 1,
-        'value' => $this->created['product']['AW1510-54L'],
+        'entity_id' => $this->created['product']['AW1510-54L'],
         'collection_id' => $this->created['collection']['Featured products']
     ),
     array(
         'weight' => 3,
         'status' => 1,
-        'value' => $this->created['product']['T0356143605100'],
+        'entity_id' => $this->created['product']['T0356143605100'],
         'collection_id' => $this->created['collection']['Featured products']
     ),
 );
