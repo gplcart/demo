ccd591fb93fe719717389d838ab5dd9b8783f1ef
GplCart <gplcart.software@gmail.com>
2018-03-09 16:09:39 +0200

    Remove CLI commands

--- Main.php
+++ Main.php
@@ -35,39 +35,6 @@ class Main
     }
 
     /**
-     * Implements hook "cli.route.list"
-     * @param array $routes
-     */
-    public function hookCliRouteList(array &$routes)
-    {
-        $routes['demo-create'] = array(
-            'description' => 'Populate a store with a demo content', // @text
-            'usage' => array(
-                'gplcart demo-create -h',
-                'gplcart demo-create [--package=<string> --store=<int>]'
-            ),
-            'options' => array(
-                '--package' => 'A package ID used as an source [default: default]', // @text
-                '--store' => 'A numeric ID of the store you want to create demo content for [default: 1]' // @text
-            ),
-            'handlers' => array(
-                'controller' => array('gplcart\\modules\\demo\\controllers\\Cli', 'createCli')
-            )
-        );
-
-        $routes['demo-delete'] = array(
-            'description' => 'Delete all created demo content from a store', // @text
-            'options' => array(
-                '--package' => 'A package ID used as an source [default: default]', // @text
-                '--store' => 'A numeric ID of the store you want to create demo content for [default: 1]' // @text
-            ),
-            'handlers' => array(
-                'controller' => array('gplcart\\modules\\demo\\controllers\\Cli', 'deleteCli')
-            )
-        );
-    }
-
-    /**
      * Create a demo content
      * @param integer $store_id
      * @param string $handler_id
